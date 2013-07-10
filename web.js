d496c59986da4bd3fbbc4f8b78ea2a33799391c6
blbannister <blbannister@gmail.com>
2013-07-10 02:17:56 +0000

    and again

--- web.js
+++ web.js
@@ -4,8 +4,8 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buffer = new Buffer(fs.readFileSync(index.html);
-  response.send(buf.toString('utf-8'));
+  var buffer = new Buffer(fs.readFileSync(index.html));
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
